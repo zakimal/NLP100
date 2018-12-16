cc1c68dd3a62158f5cec5113d6fddbd1c2fd147f
zakimal <oza777you@gmail.com>
2018-12-17 07:15:54 +0900

    Done 11

---
+++ src/NLP11.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2{
+		log.Fatalf("%s: Usage: %s <file>\n", os.Args[0])
+	}
+	fp, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer fp.Close()
+	reader := bufio.NewReader(fp)
+	for {
+		r, _, _ := reader.ReadRune()
+		if r == '\t' {
+			r = ' '
+		}
+		if r == 0 {
+			break
+		}
+		fmt.Printf(string(r))
+	}
+	fmt.Println()
+}
